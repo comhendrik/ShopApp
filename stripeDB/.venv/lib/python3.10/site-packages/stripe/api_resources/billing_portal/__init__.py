41ede6a6088ba5efe89f4d0967d141c36a841041
comhendrik <76622601+comhendrik@users.noreply.github.com>
2022-05-20 15:03:21 +0200

    Delete stripeDB/.venv directory

--- stripeDB/.venv/lib/python3.10/site-packages/stripe/api_resources/billing_portal/__init__.py
+++
@@ -1,7 +0,0 @@
-# File generated from our OpenAPI spec
-from __future__ import absolute_import, division, print_function
-
-# flake8: noqa
-
-from stripe.api_resources.billing_portal.configuration import Configuration
-from stripe.api_resources.billing_portal.session import Session
